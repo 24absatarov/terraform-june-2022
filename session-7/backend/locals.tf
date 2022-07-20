8ccd7234b21df25abbf4d15f82cb8e2f5ff325f0
24absatarov <24absatarov@gmail.com>
2022-07-20 23:57:53 +0000

    added new folders

---
+++ session-7/backend/locals.tf
@@ -0,0 +1,17 @@
+locals {
+    name = 
+}
+
+
+# Why do we need local values?
+# Naming and Tagging Standards
+# Why do we need Tags actually? to specify, cost-allocation tags, 
+
+# Naming Standard:
+# 1. What Cloud Provider? = aws
+# 2. What is the account? = non-prod, prod
+# 3. What is the environment? = dev, qa, stage, prod
+# 4. What is the region? = us-west-2, us-east-1
+# 5. What is the project? = wordpress, matrix, apple
+
+# How does it look?
