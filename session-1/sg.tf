2444ad5de3228f1b737c2bf8bd78794899a7dc41
24absatarov <24absatarov@gmail.com>
2022-07-25 21:14:44 +0000

    updated files

--- session-1/sg.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_security_group" "main" {
-    name        = "SecurityGroup"
-    description = "This is my sg for my EC2"
-}
-
-# Argument = Terraform defines, Unique
-# Value    = Author defines, Does not need to be unique
