8e8eea703f29860b250384aa8efdbc6103a62b04
24absatarov <24absatarov@gmail.com>
2022-07-26 01:19:26 +0000

    bucket name added as a tag

---
+++ session-10/main.tf
@@ -0,0 +1,16 @@
+# module "s3" {
+#     source = "github.com/aKumoSolutions/terraform-june-2022/modules/s3/"
+#     env = "dev"
+#     my_name = "daniel"
+# }
+# module "vpc" {
+#     source = "terraform-aws-modules/vpc/aws"
+#     version = "3.14.2"
+#     cidr = "10.0.0.0/16"
+# }
+module "ec2" {
+    source = "github.com/24absatarov/terrraform-june-2022/modules/ec2/"
+    env = "dev"
+    ami = "ami-0cff7528ff583bf9a"
+    instance_type = "t2.micro"
+}
